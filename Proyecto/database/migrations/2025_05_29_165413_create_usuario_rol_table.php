8bdcde0af4d61d0e483dff010a3dcbeeb9cfdc43
Santiago Echavarria <42123750+N0TSANTIAGO@users.noreply.github.com>
2025-05-29 12:10:22 -0500

    Creacion de migraciones y vista

--- Proyecto/database/migrations/2025_05_29_165413_create_usuario_rol_table.php
+++ Proyecto/database/migrations/2025_05_29_165413_create_usuario_rol_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('asignaturas', function (Blueprint $table) {
+        Schema::create('usuario_rol', function (Blueprint $table) {
             $table->id();
-            $table->string('codigo')->unique();
-            $table->string('descripcion');
-            $table->integer('creditos');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('asignaturas');
+        Schema::dropIfExists('usuario_rol');
     }
 };
